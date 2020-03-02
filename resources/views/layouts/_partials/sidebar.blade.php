a2c9194b115621c2ca2c4645e577a4b464fdc0b1
iqbalabdurrazaq <mancunianmerchant816@gmail.com>
2020-03-02 16:23:01 +0700

    sidebar

--- resources/views/layouts/_partials/sidebar.blade.php
+++ resources/views/layouts/_partials/sidebar.blade.php
@@ -33,62 +33,53 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="nav-link" ><i class="fas fa-user-clock"></i> <span>Attandance</span></a>
+                    <a href="#" class="nav-link has-dropdown" ><i class="fas fa-user-clock"></i> <span>Attendance</span></a>
                     <ul class="dropdown-menu">
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Department</span></a>
+                            <a href="#" class="nav-link" > <span>Manage Work Shift</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Designation</span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Termination</span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
+                            <a href="#" class="nav-link" > <span>Report</span></a>
                         </li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="nav-link" ><i class="fas fa-money-bill-alt"></i> <span>Payroll</span></a>
+                    <a href="#" class="nav-link has-dropdown" ><i class="fas fa-money-bill-alt"></i> <span>Payroll</span></a>
                     <ul class="dropdown-menu">
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Department</span></a>
+                            <a href="#" class="nav-link" > <span>Tax Rule Setup</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Designation</span></a>
+                            <a href="#" class="nav-link" > <span>Late Configuration</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
+                            <a href="#" class="nav-link" > <span>Allowance</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Termination</span></a>
+                            <a href="#" class="nav-link" > <span>Deduction</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
+                            <a href="#" class="nav-link" > <span>Monthly Pay Grade</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Hourly Pay Grade</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Payment History</span></a>
                         </li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="nav-link" ><i class="fas fa-street-view"></i> <span>Recruitement</span></a>
+                    <a href="#" class="nav-link has-dropdown" ><i class="fas fa-street-view"></i> <span>Recruitement</span></a>
                     <ul class="dropdown-menu">
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Department</span></a>
+                            <a href="#" class="nav-link" > <span>Job Post</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Designation</span></a>
+                            <a href="#" class="nav-link" > <span>Job Candidate</span></a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Termination</span></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
+                            <a href="#" class="nav-link" > <span>Notice Board</span></a>
                         </li>
                     </ul>
                 </li>
