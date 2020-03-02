bc4fd3396d53f00fb9a81b84461b4a02058831b0
iqbalabdurrazaq <mancunianmerchant816@gmail.com>
2020-03-02 16:00:31 +0700

    sidebar

--- resources/views/layouts/_partials/sidebar.blade.php
+++ resources/views/layouts/_partials/sidebar.blade.php
@@ -12,34 +12,85 @@
                 <li class="dropdown {{ Request::segment(2) === 'home'? 'active' : null }}">
                     <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Dashboard</span></a>
                 </li>
-                <li class="dropdown {{ Request::segment(2) === 'employe'? : null }}">
-                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Employee Management</span></a>
-                    <ul>
-                        <li class="dropdown {{ Request::segment(2) === 'department'? : null }}">
-                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Department</span></a>
+                <li class="dropdown">
+                    <a href="#" class="nav-link has-dropdown" ><i class="fas fa-users-cog"></i> <span>Employee Management</span></a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Department</span></a>
                         </li>
-                        <li class="dropdown {{ Request::segment(2) === 'designation'? : null }}">
-                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Designation</span></a>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Designation</span></a>
                         </li>
-                        <li class="dropdown {{ Request::segment(2) === 'manageemployee'? : null }}">
-                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Manage Employee</span></a>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
                         </li>
-                        <li class="dropdown {{ Request::segment(2) === 'termination'? : null }}">
-                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Termination</span></a>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Termination</span></a>
                         </li>
-                        <li class="dropdown {{ Request::segment(2) === 'employeepermanent'? : null }}">
-                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Employee Permanent</span></a>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
                         </li>
                     </ul>
                 </li>
-                <li class="dropdown {{ Request::segment(2) === 'attandance'? 'active' : null }}">
-                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Attandance</span></a>
+                <li class="dropdown">
+                    <a href="#" class="nav-link" ><i class="fas fa-user-clock"></i> <span>Attandance</span></a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Department</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Designation</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Termination</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
+                        </li>
+                    </ul>
                 </li>
-                <li class="dropdown {{ Request::segment(2) === 'payroll'? 'active' : null }}">
-                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Payroll</span></a>
+                <li class="dropdown">
+                    <a href="#" class="nav-link" ><i class="fas fa-money-bill-alt"></i> <span>Payroll</span></a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Department</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Designation</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Termination</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
+                        </li>
+                    </ul>
                 </li>
-                <li class="dropdown {{ Request::segment(2) === 'recruitement'? 'active' : null }}">
-                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Recruitement</span></a>
+                <li class="dropdown">
+                    <a href="#" class="nav-link" ><i class="fas fa-street-view"></i> <span>Recruitement</span></a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Department</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Designation</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Manage Employee</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Termination</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link" > <span>Employee Permanent</span></a>
+                        </li>
+                    </ul>
                 </li>
           </ul>
           </aside>
