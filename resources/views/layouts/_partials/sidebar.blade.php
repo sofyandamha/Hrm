b6d7939c7fb0dbc8db1b0902b03c39f624d961b0
iqbalabdurrazaq <mancunianmerchant816@gmail.com>
2020-03-02 15:45:37 +0700

    sidebar

--- resources/views/layouts/_partials/sidebar.blade.php
+++ resources/views/layouts/_partials/sidebar.blade.php
@@ -12,5 +12,34 @@
                 <li class="dropdown {{ Request::segment(2) === 'home'? 'active' : null }}">
                     <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Dashboard</span></a>
                 </li>
+                <li class="dropdown {{ Request::segment(2) === 'employe'? : null }}">
+                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Employee Management</span></a>
+                    <ul>
+                        <li class="dropdown {{ Request::segment(2) === 'department'? : null }}">
+                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Department</span></a>
+                        </li>
+                        <li class="dropdown {{ Request::segment(2) === 'designation'? : null }}">
+                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Designation</span></a>
+                        </li>
+                        <li class="dropdown {{ Request::segment(2) === 'manageemployee'? : null }}">
+                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Manage Employee</span></a>
+                        </li>
+                        <li class="dropdown {{ Request::segment(2) === 'termination'? : null }}">
+                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Termination</span></a>
+                        </li>
+                        <li class="dropdown {{ Request::segment(2) === 'employeepermanent'? : null }}">
+                            <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Employee Permanent</span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown {{ Request::segment(2) === 'attandance'? 'active' : null }}">
+                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Attandance</span></a>
+                </li>
+                <li class="dropdown {{ Request::segment(2) === 'payroll'? 'active' : null }}">
+                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Payroll</span></a>
+                </li>
+                <li class="dropdown {{ Request::segment(2) === 'recruitement'? 'active' : null }}">
+                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Recruitement</span></a>
+                </li>
           </ul>
           </aside>
