b356fc0150b65608c10bf78e3ec71502027d144b
muamarhm <muamarhumaidi@gmail.com>
2020-03-02 15:01:45 +0700

    second commit

--- resources/views/layouts/_partials/sidebar.blade.php
+++ resources/views/layouts/_partials/sidebar.blade.php
@@ -10,22 +10,7 @@
 
             <li class="menu-header">Menu</li>
                 <li class="dropdown {{ Request::segment(2) === 'home'? 'active' : null }}">
-                    <a href="{{ route('home') }}" class="nav-link" ><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+                    <a href="#" class="nav-link" ><i class="fas fa-fire"></i> <span>Dashboard</span></a>
                 </li>
-            @if (auth()->user()->role_id  == 1)
-                <li class="dropdown {{ Request::segment(2) === 'resume'? 'active' : null }}">
-                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users"></i> <span>Administrator</span></a>
-                    <ul class="dropdown-menu">
-                    <li class="{{  Request::segment(2) === 'resume' ? 'active' : null }}"><a class="nav-link" href="{{ route('resume') }}">Data Price</a></li>
-                    </ul>
-                </li>
-            @elseif(auth()->user()->role_id  == 2)
-                <li class="dropdown {{ Request::segment(2) === 'addprice'? 'active' : null }}">
-                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-bullhorn"></i> <span>Vendor</span></a>
-                    <ul class="dropdown-menu">
-                    <li class="{{  Request::segment(2) === 'addprice' ? 'active' : null }}"><a class="nav-link" href="{{ route('add_price') }}">Add Price</a></li>
-                    </ul>
-                </li>
-            @endif
           </ul>
           </aside>
