9f9b58c98d73566d0b90021d987ba7542a7bdee0
muamar <muamarhumaidi@gmail.com>
2020-03-15 13:11:43 +0700

    test

--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -8,7 +8,7 @@
     <div class="col-12 col-md-6 col-lg-6">
       <div class="card">
         <div class="card-header">
-          <h4>Add New Department</h4>
+          <h4>Add New Departement</h4>
         </div>
         <div class="card-body">
             <div class="form-group">
